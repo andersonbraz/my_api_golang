d0b0ebfab6c7635fd64dcfd6e44668e0d4459d17
Anderson Braz <braz_anderson@msn.com>
2020-05-25 11:39:01 -0300

    Up Files

--- cli/cmd/check.go
+++ cli/cmd/check.go
@@ -24,13 +24,8 @@ import (
 // checkCmd represents the check command
 var checkCmd = &cobra.Command{
 	Use:   "check",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Check Settings or Eviroment Variables",
+	Long:  `Execute comand: agent check --config mongodb`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("check called")
 	},
