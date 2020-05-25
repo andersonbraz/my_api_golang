951342247b2a8e9652fceb9b96eb1eb7ce8187d8
Anderson Braz <braz_anderson@msn.com>
2020-05-24 22:01:41 -0300

    Up Files

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -58,7 +58,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.agent.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "./config/todo.yml", "")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
