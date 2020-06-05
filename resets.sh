e344ead5836e6bd33acf2d47a4aa3254f96cc22d
Anderson Braz <braz_anderson@msn.com>
2020-06-05 12:19:44 -0300

    Up Files

--- resets.sh
+++ resets.sh
@@ -8,5 +8,6 @@ docker container prune -f
 docker network prune -f
 docker volume prune -f
 docker images prune
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 docker container ls -a
 docker ps
